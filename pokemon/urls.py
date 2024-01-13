9db85a86083a352d959717c926698cd085a1234e
BrailyRs03 <brailyrs03@gmail.com>
2024-01-13 00:01:10 -0400

    Funciones de pokemons y regiones terminadas

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('pokelist/', views.pokelist, name='pokelist'),
     path('add_pokemon/', views.add_pokemon, name="add_pokemon"),
+    path('pokedelete/<int:id>/', views.pokedelete, name='pokedelete'),
+    path('pokedit/<int:id>/', views.edit_pokemon, name='pokedit'),
+    # Regiones
+    path('regiones/', views.regionlist, name="regiones"),
+    path('add_region/', views.add_region, name="add_region"),
+    path('delete_region/<int:id>/',views.delete_region, name="delete_region"),
+    path('edit_region/<int:id>/', views.edit_region, name='edit_region')
 ]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from . import models
 from . import form
+from django.contrib import messages
+from django.utils.html import format_html
 
 def index(request):
     # Obtiene todas los regiones para mostrar en el formulario
@@ -21,18 +23,84 @@ def index(request):
     return render(request, "index.html", {"pokemon": pokemons, "regions": regions})
 
 
+# Mantenimiento de Pokemons
+def pokelist(request):
+    
+    pokemons = models.Pokemon.objects.all()
+    
+    return render(request, "pokemons/lista_pokemon.html", {"pokelist":pokemons})
+
 def add_pokemon(request):
     
     if request.method == "GET":
         form_pokemon = form.PokemonForm(request.GET)
-        return render(request, "pokeform.html", {"form":form_pokemon})
+        return render(request, "pokemons/pokeform.html", {"form":form_pokemon})
     
     if request.method == "POST":
         form_pokemon = form.PokemonForm(request.POST, request.FILES)
         if form_pokemon.is_valid():
             form_pokemon.save()
-            return redirect("home")
+            return redirect("pokelist")
         return render(request, "pokeform.html", {"form":form_pokemon})
-            
+
+def edit_pokemon(request, id):
+    pokemon = models.Pokemon.objects.get(id=id)
+
+    if request.method == "GET":
+        form_pokemon = form.PokemonForm(instance=pokemon)
+        return render(request, "pokemons/pokedit.html", {"form": form_pokemon, "id": id})
+
+    if request.method == "POST":
+        form_pokemon = form.PokemonForm(request.POST, request.FILES, instance=pokemon)
+        if form_pokemon.is_valid():
+            form_pokemon.save()
+            messages.success(request, 'La información del Pokémon se ha actualizado')
+        return render(request, "pokemons/pokedit.html", {"form": form_pokemon, "id": id})
+
+def pokedelete(request, id):
     
+    pokemons = models.Pokemon.objects.get(id=id)
+    if request.method == "GET":
+        pokemons.delete()
+        messages.success(request, format_html('<strong>El pokemon a sido eliminado</strong>'))
+        return redirect("pokelist")
+    # return render(request, "lista_pokemon.html", {""})
+# ! REGION
+def regionlist(request):
+    region = models.Region.objects.all()
+    return render(request, "regiones/lista_region.html", {"region":region})
+
+def add_region(request):
     
+    if request.method == "GET":
+        form_region = form.RegionForm(request.GET)
+        return render(request, "regiones//regionform.html", {"form":form_region})
+    
+    if request.method == "POST":
+        form_region = form.RegionForm(request.POST)
+        if form_region.is_valid():
+            form_region.save()
+        return redirect("regiones")
+
+def edit_region(request, id):
+    region = models.Region.objects.get(id=id)
+    
+    if request.method == "GET":
+        form_region = form.RegionForm(instance=region)
+        return render(request, "regiones/edit_region.html", {"form":form_region, "id":id})
+    
+    if request.method == "POST":
+        form_region = form.RegionForm(request.POST, instance=region)
+        if form_region.is_valid():
+            form_region.save()
+            messages.success(request, 'Se a actualizado la region.')
+            return redirect("regiones")
+    
+def delete_region(request, id):
+    region = models.Region.objects.get(id=id)
+    if request.method == "GET":
+        region.delete()
+        messages.success(request, 'Se a Eliminado la region')
+        return redirect("regiones")
+    
+# ! TIPOS
